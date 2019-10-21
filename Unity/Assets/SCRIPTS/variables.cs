3d2f164ba647ecbba5a5b72314a74a7f143cedbe
antonio-ruiz-roldan <leketembe@gmail.com>
2019-10-21 20:20:07 +0200

    Subida del ejercicio

---
+++ Unity/Assets/SCRIPTS/variables.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour {
+
+	public string miNombre;
+	public int miEdad;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Mi nombre es: " + miNombre);
+		Debug.Log ("Mi edad es: " + miEdad)
+
+		for (var i = 0; i < miEdad; i+) {
+			Debug.Log ("Contador edad: " + i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
